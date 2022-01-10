2129a96017d7f85f155845421f0dbad09c8f8bcc
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2022-01-10 18:10:00 +0100

    ✨  Update Home page

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
+import "./home.scss";
+import Table from "../../components/Table/Table";
+import { useState } from "react";
+import data from "../../data/mockedEmployees.json"
+
 
 export default function Home() {
- return (
-  <div>
-   C'est la homepage
-  </div>
- )
+	const [foundEmployees, setFoundEmployees] = useState("");
+
+	const handleSearch = (event) => {
+		const query = event.target.value;
+		if (query.length >= 3) {
+			const results = data.filter((employee) => {
+				return (
+					employee.lastname.toLowerCase().startsWith(query.toLowerCase()) ||
+					employee.firstname.toLowerCase().startsWith(query.toLowerCase()) ||
+					employee.department.toLowerCase().startsWith(query.toLowerCase())
+				);
+			});
+			setFoundEmployees(results);
+		} else {
+			setFoundEmployees(data);
+		}
+	};
+	return (
+		<main className="container">
+			<header className="container__header">
+				<h1 className="container__title">Dashboard</h1>
+				<input
+					type="search"
+					placeholder="Search..."
+					className="container__input"
+					onChange={handleSearch}
+				/>
+			</header>
+			<p>{data.id}</p>
+			<Table data={foundEmployees || data} />
+		</main>
+	);
 }
