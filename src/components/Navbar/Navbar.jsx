98186267add1f9a2a516c6b464aed7a4c30d9c34
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2022-01-10 18:09:12 +0100

    ⚡️  Add actions on navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import Logo from "./logo.png";
+import { ReactComponent as UserIcon } from "./usericon.svg";
+import "./navbar.scss";
 
-export default function Navbar() {
- return (
-  <div>
-   
-  </div>
- )
+export default function Navbar({onOpen}) {
+
+	return (
+		<nav className="navbar">
+			<img className="navbar__logo" src={Logo} alt="" />
+			<button className="navbar__btn" onClick={onOpen}>
+				<UserIcon className="navbar__btn__icon" />
+				<span>New employee</span>
+			</button>
+		</nav>
+	);
 }
