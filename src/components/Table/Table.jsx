9512c4eb45652c70bb47c7e4f18196f02ed3ce54
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2022-01-10 18:09:33 +0100

    ✨ Create table component

---
+++ src/components/Table/Table.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import "./table.scss";
+import DataTable from "react-data-table-component";
+const columns = [
+	{
+		name: "First Name",
+		selector: (row) => row.firstname,
+		sortable: true,
+	},
+	{
+		name: "Last Name",
+		selector: (row) => row.lastname,
+		sortable: true,
+	},
+	{
+		name: "Start Date",
+		selector: (row) => row.startDate,
+		sortable: true,
+	},
+	{
+		name: "Department",
+		selector: (row) => row.department,
+		sortable: true,
+	},
+	{
+		name: "Date of Birth",
+		selector: (row) => row.dateOfBirth,
+		sortable: true,
+	},
+	{
+		name: "Street",
+		selector: (row) => row.street,
+		sortable: true,
+	},
+	{
+		name: "City",
+		selector: (row) => row.city,
+		sortable: true,
+	},
+	{
+		name: "State",
+		selector: (row) => row.state,
+		sortable: true,
+	},
+	{
+		name: "Zipcode",
+		selector: (row) => row.zipcode,
+		sortable: true,
+	},
+];
+
+
+/* Temporary data */
+
+export default function Table({ data }) {
+	return <DataTable columns={columns} data={data} highlightOnHover pagination striped></DataTable>;
+}
