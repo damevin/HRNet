3d89ef85d59382564340ee1237dc41ad2a5473a9
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2022-01-10 18:08:41 +0100

    🎨  Import and make modal working on App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import Router from "./router";
 import Navbar from "./components/Navbar/Navbar.jsx";
+import Modal from "./components/Modal/Modal";
+import { useState } from "react";
 
 function App() {
+	const [modal, setModal] = useState(false);
+
+	const toggleModal = () => {
+		setModal(!modal);
+	};
+
 	return (
 		<div className="App">
-			<Navbar />
+			<Navbar onOpen={toggleModal} />
 			<Router />
+			{modal && <Modal onClose={toggleModal}/>}
 		</div>
 	);
 }
