aab5668aa34b7c6ced78393da8882c882c8d699d
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2022-01-11 11:36:23 +0100

    ♻️  Remove some logic in App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import Router from "./router";
 import Navbar from "./components/Navbar/Navbar.jsx";
-import Modal from "./components/Modal/Modal";
-import { useState } from "react";
 
 function App() {
-	const [modal, setModal] = useState(false);
-
-	const toggleModal = () => {
-		setModal(!modal);
-	};
-
 	return (
 		<div className="App">
-			<Navbar onOpen={toggleModal} />
+			<Navbar />
 			<Router />
-			{modal && <Modal onClose={toggleModal}/>}
 		</div>
 	);
 }
