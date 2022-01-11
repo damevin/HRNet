4ac5c7649b94c40c7d8022a9a084affc2f9fb8dd
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2022-01-11 11:35:40 +0100

    🚚  Create new route for create an employee

--- src/router.js
+++ src/router.js
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./containers/Home/Home";
+import CreateUser from "./containers/CreateUser/CreateUser"
 import Error from "./containers/Error/Error";
 
 const Router = () => {
 	return (
 		<Routes>
 			<Route path="/" element={<Home />}></Route>
+			<Route path="/createEmployee" element={<CreateUser />}></Route>
 			<Route path="*" element={<Error />}></Route>
 		</Routes>
 	);
