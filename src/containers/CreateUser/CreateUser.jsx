e300fa28e2f41a996530cb1c07aa1c6dfc8e2f66
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2022-01-11 11:37:23 +0100

    🚧 WIP: Create User page

---
+++ src/containers/CreateUser/CreateUser.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import FormEmployee from "../../components/FormEmployee/FormEmployee";
+
+export default function CreateUser() {
+	return (
+		<div className="container">
+			<h1>Create new employee</h1>
+			<FormEmployee />
+		</div>
+	);
+}
