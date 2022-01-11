aa79910c4f9eed14ad9edb2bc6c675a9e30f9094
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2022-01-11 11:36:44 +0100

    🚧 WIP: Form employee for create an employee in database

---
+++ src/components/FormEmployee/FormEmployee.jsx
@@ -0,0 +1,122 @@
+import React from "react";
+import { useRef } from "react";
+import "./formemployee.scss";
+
+export default function FormEmployee() {
+	const employeeForm = {
+		dateOfBirth: useRef(),
+		city: useRef(),
+		department: useRef(),
+		firstname: useRef(),
+		lastname: useRef(),
+		startDate: useRef(),
+		state: useRef(),
+		street: useRef(),
+		zipcode: useRef(),
+	};
+
+	const handleEmployeeForm = (event) => {
+		let employees = JSON.parse(localStorage.getItem("employees")) || [];
+		let employee = {};
+
+		event.preventDefault();
+
+		for (const key in employeeForm) {
+			if (Object.hasOwnProperty.call(employeeForm, key)) {
+				const item = employeeForm[key].current.value;
+				employee[key] = item;
+			}
+		}
+
+		employees.push(employee);
+		localStorage.setItem("employees", JSON.stringify(employees));
+
+		event.target.reset();
+		/** TO DO
+		 * - Clear form
+		 * - Display modal when succeeds
+		 *  */
+	};
+
+	return (
+		<form onSubmit={handleEmployeeForm} className="form">
+			<div className="form__section">
+				<label className="form__section__label">First Name</label>
+				<input
+					className="form__section__input"
+					required
+					type="text"
+					ref={employeeForm.firstname}
+				></input>
+			</div>
+			<div className="form__section">
+				<label className="form__section__label">Last Name</label>
+				<input
+					className="form__section__input"
+					required
+					type="text"
+					ref={employeeForm.lastname}
+				></input>
+			</div>
+			<div className="form__section">
+				<label className="form__section__label">Date of Birth</label>
+				<input
+					className="form__section__input"
+					required
+					type="date"
+					ref={employeeForm.dateOfBirth}
+				></input>
+			</div>
+			<div className="form__section">
+				<label className="form__section__label">Start Date</label>
+				<input
+					className="form__section__input"
+					required
+					type="date"
+					ref={employeeForm.startDate}
+				></input>
+			</div>
+			<div className="form__section">
+				<label className="form__section__label">Street</label>
+				<input className="form__section__input" required type="text" ref={employeeForm.street}></input>
+			</div>
+			<div className="form__section">
+				<label className="form__section__label">City</label>
+				<input className="form__section__input" required type="text" ref={employeeForm.city}></input>
+			</div>
+			<div className="form__section">
+				<label className="form__section__label">State</label>
+				{/* To do map method with all avaible cities */}
+				<select name="cars" id="cars" form="carform" required ref={employeeForm.state}>
+					<option value="volvo">Volvo</option>
+					<option value="saab">Saab</option>
+					<option value="opel">Opel</option>
+					<option value="audi">Audi</option>
+				</select>
+			</div>
+			<div className="form__section">
+				<label className="form__section__label">Zip Code</label>
+				<input
+					className="form__section__input"
+					required
+					type="number"
+					ref={employeeForm.zipcode}
+				></input>
+			</div>
+			<div className="form__section">
+				<label className="form__section__label">Department</label>
+				{/* To do map method with all avaible departement */}
+				<select name="cars" id="cars" form="carform" ref={employeeForm.department}>
+					<option value="volvo">Volvo</option>
+					<option value="saab">Saab</option>
+					<option value="opel">Opel</option>
+					<option value="audi">Audi</option>
+				</select>
+			</div>
+
+			<button className="form__btn" type="submit">
+				Save
+			</button>
+		</form>
+	);
+}
