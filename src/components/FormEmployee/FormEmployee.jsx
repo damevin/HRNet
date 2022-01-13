b87dbb4948a582046c2272fb2b1cb5237d2eaf64
raphaeldamevin <72107589+damevin@users.noreply.github.com>
2022-01-13 15:24:24 +0100

    🚧  WIP: Display modal component when form is valid

--- src/components/FormEmployee/FormEmployee.jsx
+++ src/components/FormEmployee/FormEmployee.jsx
@@ -1,8 +1,16 @@
 import React from "react";
-import { useRef } from "react";
+import { useRef, useState } from "react";
+import { Modal } from "@damevin/reactlightmodal";
+import { Link } from "react-router-dom";
 import "./formemployee.scss";
 
 export default function FormEmployee() {
+	const [isOpen, setOpen] = useState();
+
+	const toggleModal = () => {
+		setOpen(!isOpen);
+	};
+
 	const employeeForm = {
 		dateOfBirth: useRef(),
 		city: useRef(),
@@ -30,7 +38,7 @@ export default function FormEmployee() {
 
 		employees.push(employee);
 		localStorage.setItem("employees", JSON.stringify(employees));
-
+		toggleModal();
 		event.target.reset();
 		/** TO DO
 		 * - Clear form
@@ -39,84 +47,104 @@ export default function FormEmployee() {
 	};
 
 	return (
-		<form onSubmit={handleEmployeeForm} className="form">
-			<div className="form__section">
-				<label className="form__section__label">First Name</label>
-				<input
-					className="form__section__input"
-					required
-					type="text"
-					ref={employeeForm.firstname}
-				></input>
-			</div>
-			<div className="form__section">
-				<label className="form__section__label">Last Name</label>
-				<input
-					className="form__section__input"
-					required
-					type="text"
-					ref={employeeForm.lastname}
-				></input>
-			</div>
-			<div className="form__section">
-				<label className="form__section__label">Date of Birth</label>
-				<input
-					className="form__section__input"
-					required
-					type="date"
-					ref={employeeForm.dateOfBirth}
-				></input>
-			</div>
-			<div className="form__section">
-				<label className="form__section__label">Start Date</label>
-				<input
-					className="form__section__input"
-					required
-					type="date"
-					ref={employeeForm.startDate}
-				></input>
-			</div>
-			<div className="form__section">
-				<label className="form__section__label">Street</label>
-				<input className="form__section__input" required type="text" ref={employeeForm.street}></input>
-			</div>
-			<div className="form__section">
-				<label className="form__section__label">City</label>
-				<input className="form__section__input" required type="text" ref={employeeForm.city}></input>
-			</div>
-			<div className="form__section">
-				<label className="form__section__label">State</label>
-				{/* To do map method with all avaible cities */}
-				<select name="cars" id="cars" form="carform" required ref={employeeForm.state}>
-					<option value="volvo">Volvo</option>
-					<option value="saab">Saab</option>
-					<option value="opel">Opel</option>
-					<option value="audi">Audi</option>
-				</select>
-			</div>
-			<div className="form__section">
-				<label className="form__section__label">Zip Code</label>
-				<input
-					className="form__section__input"
-					required
-					type="number"
-					ref={employeeForm.zipcode}
-				></input>
-			</div>
-			<div className="form__section">
-				<label className="form__section__label">Department</label>
-				{/* To do map method with all avaible departement */}
-				<select name="cars" id="cars" form="carform" ref={employeeForm.department}>
-					<option value="volvo">Volvo</option>
-					<option value="saab">Saab</option>
-					<option value="opel">Opel</option>
-					<option value="audi">Audi</option>
-				</select>
-			</div>
+		<>
+			<form onSubmit={handleEmployeeForm} className="form">
+				<div className="form__section">
+					<label className="form__section__label">First Name</label>
+					<input
+						className="form__section__input"
+						required
+						type="text"
+						ref={employeeForm.firstname}
+					></input>
+				</div>
+				<div className="form__section">
+					<label className="form__section__label">Last Name</label>
+					<input
+						className="form__section__input"
+						required
+						type="text"
+						ref={employeeForm.lastname}
+					></input>
+				</div>
+				<div className="form__section">
+					<label className="form__section__label">Date of Birth</label>
+					<input
+						className="form__section__input"
+						required
+						type="date"
+						ref={employeeForm.dateOfBirth}
+					></input>
+				</div>
+				<div className="form__section">
+					<label className="form__section__label">Start Date</label>
+					<input
+						className="form__section__input"
+						required
+						type="date"
+						ref={employeeForm.startDate}
+					></input>
+				</div>
+				<div className="form__section">
+					<label className="form__section__label">Street</label>
+					<input className="form__section__input" required type="text" ref={employeeForm.street}></input>
+				</div>
+				<div className="form__section">
+					<label className="form__section__label">City</label>
+					<input className="form__section__input" required type="text" ref={employeeForm.city}></input>
+				</div>
+				<div className="form__section">
+					<label className="form__section__label">State</label>
+					{/* To do map method with all avaible states */}
+					<select name="cars" id="cars" form="carform" required ref={employeeForm.state}>
+						<option value="volvo">Volvo</option>
+						<option value="saab">Saab</option>
+						<option value="opel">Opel</option>
+						<option value="audi">Audi</option>
+					</select>
+				</div>
+				<div className="form__section">
+					<label className="form__section__label">Zip Code</label>
+					<input
+						className="form__section__input"
+						required
+						type="number"
+						ref={employeeForm.zipcode}
+					></input>
+				</div>
+				<div className="form__section">
+					<label className="form__section__label">Department</label>
+					{/* To do map method with all avaible departement */}
+					<select name="cars" id="cars" form="carform" ref={employeeForm.department}>
+						<option value="volvo">Volvo</option>
+						<option value="saab">Saab</option>
+						<option value="opel">Opel</option>
+						<option value="audi">Audi</option>
+					</select>
+				</div>
 
-			<button className="form__btn" type="submit">
-				Save
-			</button>
-		</form>
+				<button className="form__btn" type="submit">
+					Save
+				</button>
+			</form>
+
+			<Modal
+				isOpen={isOpen}
+				canClose={true}
+				closeBtnContent={<>X</>}
+				footerContent={
+					<>
+						<Link to="/">
+							<button>Employees list</button>
+						</Link>
+						<button>Add another</button>
+					</>
+				}
+				title="Great!"
+				haveFooter={true}
+				modalContent={<p>Your employee has been correctly added to your employees list!</p>}
+				toggleModal={toggleModal}
+			></Modal>
+		</>
 	);
 }
