577faa37ba4dc8df9215470fd34117f5b61117b3
raphaeldamevin <72107589+damevin@users.noreply.github.com>
2022-01-14 18:21:07 +0100

    ✨  Improve create user page

--- src/containers/CreateUser/CreateUser.jsx
+++ src/containers/CreateUser/CreateUser.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import FormEmployee from "../../components/FormEmployee/FormEmployee";
+import "./createuser.scss";
 
 export default function CreateUser() {
 	return (
-		<div className="container">
-			<h1>Create new employee</h1>
+		<div className="wrapper">
+			<h1 className="container__title">Create new employee</h1>
 			<FormEmployee />
 		</div>
 	);
