3f9516c047b518e60c2cce4a288599cf421564bb
raphaeldamevin <72107589+damevin@users.noreply.github.com>
2022-01-20 12:09:13 +0100

    ✨ Create error page

--- src/containers/Error/Error.jsx
+++ src/containers/Error/Error.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import "./error.scss";
+import { Link } from "react-router-dom";
+import ErrorImg from "./error.png";
 
 export default function Error() {
- return (
-  <div>
-   c'est l'erreur
-  </div>
- )
+	return (
+		<div className="container">
+			<img src={ErrorImg} className="error-img" alt="error"></img>
+			<h1 className="title">Oups....</h1>
+			<p className="details">
+				An error occurs, please try again or go to{" "}
+				<Link className="link" to="/">
+					the homepage
+				</Link>
+			</p>
+		</div>
+	);
 }
